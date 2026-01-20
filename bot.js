85b9b9d7a7dc0fdd5f4901b3b52151da47027995
king7697 <oumar20kanta@gmail.com>
2026-01-20 10:14:52 +0000

    Initialize Telegram bot with start command

---
+++ bot.js
@@ -0,0 +1,10 @@
+const TelegramBot = require("node-telegram-bot-api");
+
+const TOKEN = process.env.BOT_TOKEN;
+const bot = new TelegramBot(TOKEN, { polling: true });
+
+bot.onText(/\/start/, (msg) => {
+  bot.sendMessage(msg.chat.id, "🤖 Bot en ligne !");
+});
+
+console.log("Bot démarré");
